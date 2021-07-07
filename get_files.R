88eef15e801226814bd3c92a976120dd43116ad7
asinayev <asinayev@gmail.com>
2021-07-07 16:09:51 +0000

    download files from kaggle

---
+++ get_files.R
@@ -0,0 +1,14 @@
+install.packages(c("devtools"))
+devtools::install_github("ldurazo/kaggler")
+library(readr)
+library(kaggler)
+library(stringr)
+
+# save your kaggle credentials from https://www.kaggle.com/USERNAME/account
+kgl_auth(creds_file = 'kaggle.json')
+
+response <- kgl_datasets_download_all(owner_dataset = "jacksoncrow/stock-market-dataset")
+download.file(response[["url"]], "data/temp.zip", mode="wb")
+unzip_list <- unzip("data/temp.zip", exdir = "data/", list=TRUE )
+stock_filenames <- unzip_list$Name[str_detect(unzip_list$Name, "stocks")]
+unzip_result <- unzip("data/temp.zip", exdir = "data/", overwrite = TRUE, files=stock_filenames )
