33123091e22a9651f3f6c3e035861fa6416681ef
asinayev <asinayev@gmail.com>
2021-07-12 17:59:45 +0000

    abstract model making

--- trade_strategy.R
+++ trade_strategy.R
@@ -1,3 +1,10 @@
+get_stock_model = function(stockdat){
+  model = lm(CloseDiff~CloseDiffLag1*CloseDiff7D+CloseDiffLag2+CloseDiffLag3,
+             data=stockdat, subset=sample=="train")
+  return(model)
+}
+
+
 strategy = function(allstocks=allstocks, stocknames=stocknames,
                     train_start, train_end, test_start, test_end, oos_start, oos_end){
   allstocks[,sample:=ifelse(Date <= train_end & Date>train_start,
@@ -8,15 +15,14 @@ strategy = function(allstocks=allstocks, stocknames=stocknames,
   x = lapply(stocknames,function(stockname){
     x=allstocks[stock==stockname]
     unique_training_samples=length(unique(x[sample=='train']$AdjClose))
-    x[,training_samples:=unique_training_samples]
     if(unique_training_samples>30){
-      model = lm(CloseDiff~CloseDiffLag1*CloseDiff7D+CloseDiffLag2+CloseDiffLag3,
-                 data=x, subset=sample=="train")
+      model = get_stock_model(x)
       x[,PredDiff:=predict(model,x)]
     } else {
       x[,PredDiff:=NA]
     }
     error_improvement = pct_diff(x[sample=='test',mean(abs(CloseDiff-mean(CloseDiff)))], x[sample=='test',mean(abs(PredDiff-CloseDiff))])
+    x[,training_samples:=unique_training_samples]
     x[,error_improvement:=error_improvement]
     return(x[sample=='oos'])
   })
