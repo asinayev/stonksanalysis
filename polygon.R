9b8e0aceda114f2b238337664997f9fa5850d459
asinayev <asinayev@gmail.com>
2021-07-23 04:23:39 +0000

    adding stock financials

--- polygon.R
+++ polygon.R
@@ -1,4 +1,38 @@
-stocklist_from_polygon = function(key, exchange = c('XNYS','XNAS'), date = '2018-01-01'){
+hit_polygon = function(link, tries = 3,results_contain=F){
+  response = 'none'
+  while(tries>0){
+    response = tryCatch({
+      x = link %>%jsonlite::fromJSON()
+      stopifnot(x$response$status=='OK') 
+      if(results_contain!=F){
+        stopifnot(results_contain %in% response$results)
+      }
+      return(x)},
+      error = {function(x){
+        tries = tries-1
+        'none'}})
+  }
+  return(response)
+}
+
+select_field = function(response, field){
+    return(response[['results']][[field]])
+}
+
+financials_from_polygon = function( key, stockname, date, field=F){
+  response = "https://api.polygon.io/v2/reference/financials/%s?type=Y&sort=-reportPeriod&apiKey=%s" %>%
+    sprintf(stockname, key) %>%
+    hit_polygon
+  results = data.table(response$results)
+  if('dateKey' %in% names(results)){
+    results[dateKey<date][order(dateKey, decreasing = T)][1,]
+  } else {
+      return(data.table(ticker=stockname))
+    }
+}
+
+
+stocklist_from_polygon = function(key, exchange = c('XNYS','XNAS'), date = '2018-01-01', financials=F){
   resultlist=list()
   for (ex in exchange){
     go=T
@@ -6,7 +40,7 @@ stocklist_from_polygon = function(key, exchange = c('XNYS','XNAS'), date = '2018
     while(go){
       response = "https://api.polygon.io/v3/reference/tickers?market=stocks&exchange=%s&date=%s&active=true&sort=ticker&order=asc&limit=1000&apiKey=%s&ticker.gt=%s" %>%
         sprintf(ex, date, key, last_examined) %>%
-        jsonlite::fromJSON()
+        hit_polygon
       if (!is.null(response$results)){
         last_examined = response$results$ticker[nrow(response$results)]
         resultlist[paste(ex, last_examined)]= list(response$results)
@@ -16,56 +50,46 @@ stocklist_from_polygon = function(key, exchange = c('XNYS','XNAS'), date = '2018
       }
     }
   }
-  resultlist %>% rbindlist(use.names=TRUE, fill = T)
-}
-
-ticker_info_from_polygon = function( key, stockname, date, field=F, wait=F) {
-  response = 'none'
-  tries = 3
-  while(!is.list(response) & tries>0){
-    response = tryCatch({
-      "https://api.polygon.io/vX/reference/tickers/%s?date=%s&apiKey=%s" %>%
-        sprintf(stockname, date, key) %>%
-        jsonlite::fromJSON()},
-      error = {function(x){
-        tries = tries-1
-        'none'}})
-  }
-  if(wait!=F){
-    Sys.sleep(wait)
-  }
-  if(field==F){
-    return(response)
+  out = resultlist %>% 
+    rbindlist(use.names=TRUE, fill = T)
+  if(financials){
+    out$ticker %>% unlist %>%
+      unique %>%
+      parallel::mclapply(financials_from_polygon, key=key, date=date, field=F, mc.cores = 16) %>%
+      rbindlist(fill=TRUE, use.names = T) %>%
+      cbind(out) %>%
+      return
   } else {
-    return(response[['results']][[field]])
+    return(out)
   }
+} 
+
+ticker_info_from_polygon = function( key, stockname, date, field=F) {
+  "https://api.polygon.io/vX/reference/tickers/%s?date=%s&apiKey=%s" %>%
+    sprintf(stockname, date, key) %>%
+    hit_polygon %>%
+    select_field(field=field)
 }
 
 stock_history = function(stockname, start_date, end_date, key, print=F, check_ticker=T){
   if(check_ticker){
     start_cik = end_cik = 'none'
     while(start_cik == 'none'){
-      start_cik = tryCatch({
-        ticker_info_from_polygon(key, stockname, start_date, field = 'cik')},
-        error = {function(x){
-          start_date = start_date+7
-          start_cik}})
+      start_cik = ticker_info_from_polygon(key, stockname, start_date, field = 'cik')
+      start_date = start_date+7
     }
     
     while((start_cik != end_cik) & (end_date>start_date+360)){
+      end_cik = ticker_info_from_polygon(key, stockname, end_date, field = 'cik')
       end_date = end_date-7
-      end_cik = tryCatch({
-        ticker_info_from_polygon(key, stockname, end_date, field = 'cik')},
-        error = {function(x){
-          end_date = end_date-7
-          end_cik}})
     }
   }
   tries=0
-  while(tries<10){
+  while(tries<3){
     response = "https://api.polygon.io/v2/aggs/ticker/%s/range/1/day/%s/%s?adjusted=true&sort=asc&apiKey=%s" %>%
       sprintf(stockname, start_date, end_date, key) %>%
-      jsonlite::fromJSON()
+      jsonlite::fromJSON() %>%
+      hit_polygon
     tries = tries+1
     if('results' %in% names(response) & 'c' %in% names(response$results)){
       return(
@@ -98,6 +122,6 @@ sampled_data=function(key, date, nsample, exchange = c('XNYS','XNAS','XASE')){
                        key = key,
                        mc.cores = 16)
   stocklist[unlist(lapply(stocklist,is.data.frame))] %>%
-    rbindlist
+    rbindlist(fill=TRUE, use.names = T)
 }
 # stock_history('AI', as.Date('2017-01-01'),as.Date('2020-01-01'),POLYKEY)
