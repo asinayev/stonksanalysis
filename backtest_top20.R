3dc0f38430e2fccb083753b4d6d75d14f23c76e1
asinayev <asinayev@gmail.com>
2021-07-26 17:33:34 -0400

    Update backtest_top20.R

--- backtest_top20.R
+++ backtest_top20.R
@@ -2,63 +2,78 @@ setwd("stonksanalysis")
 source("prep_data.R", local=T)
 source("crossover_strategy.R", local=T)
 
-# GOod for index funds 3 vs 56 range with -.05 buy trigger and -.15 sell trigger
-
 library(tidyquant)
 
-chunksize = 500
-
-#Pull symbols and benchmark data
-# allstocks = stocklist_from_polygon(key = POLYKEY,exchange = c('XNYS','XNAS','XNGS'),
-#                                    date='2017-01-01')
-# market_caps = parallel::mclapply(allstocks$ticker, 
-#                        ticker_info_from_polygon, 
-#                        key=POLYKEY, 
-#                        date='2017-01-01', wait=.1,
-#                        field = 'market_cap', mc.cores = 4)
-# allstocks = allstocks[sapply(market_caps, function(x)length(x)==1)==T]
-# allstocks[, market_cap := unlist(market_caps)]
-
-mc2015 = c('AAPL', 'GOOG','BRK-B','XOM','MSFT','WFC','JNJ', 'WMT', "GE", 'FB', 'JPM', 
-           'PG' ,'PFE', 'VZ', 'CVX', 'ORCL', 'DIS', 'COKE', 'AMZN', 'T')
-
-fulldat = data.table(tq_get(mc2015)) %>% 
-  basic_prep(
-    rename_from=c("symbol","date","adjusted"),
-    rename_to=c("stock","Date","AdjClose")
-  )
-
-
-parameterset = expand.grid(short_range=c(3,7,14), mid_range=c(28,56,98), 
-                     buy_trigger=c(-.1,-.05,0,.05,.1,.15), sell_trigger=c(.1,.05,0,-.05,-.1,-.15), 
-                     deathcross=c(F), profit=c(-10)
+parameterset = expand.grid(short_range=c(3,7,14), mid_range=c(56), long_range=c(100,150,200),
+                     buy_trigger=c(-.1,-.05,0,.05,.1), sell_trigger=c(.1,.2,.3), 
+                     cooloff=c(), sell_after=c(180,365,550)
 ) %>% data.table
+# -.1 for buy and -.15 for sell and 7/98 for 2015 stocks
 
-test_date = seq(as.Date('2015-01-01'), as.Date('2020-01-01'), 365)
+test_date = seq(as.Date('2005-08-01'), as.Date('2019-08-01'), 365)
 
 proc_date = function(date, ins){
+  
+  financials = stocklist_from_polygon(key = POLYKEY, date = date, financials = T)
+  
+  target_companies = financials[!is.na(marketCapitalization)][order(marketCapitalization, decreasing = T)]$ticker[1:150]
+  
+  fulldat = target_companies %>% 
+    # tq_get %>% data.table %>%
+    parallel::mclapply(stock_history,
+                       start_date = date-2*365,
+                       end_date = as.Date('2022-01-01'),
+                       key = POLYKEY, check_ticker=F,
+                       mc.cores = 8) %>% rbindlist %>%
+    basic_prep(
+      rename_from=c("symbol","date","adjusted"),
+      rename_to=c("stock","Date","AdjClose")
+    )
+  
   outs = ins %>%
-    apply(1, crossoverReturns, dat=fulldat, summary=T, date = date, transaction_fee=.0001) %>%
+    apply(1, crossoverReturns, dat=fulldat, summary=T, date = date, end_date=date+365*2, transaction_fee=.0001) %>%
     rbindlist %>%
     cbind(ins)
   outs$Date = date
   outs
 }
 
-system.time({results = parallel::mclapply(dates, ins=parameterset, proc_date, mc.cores = 4)})
+system.time({results = parallel::mclapply(test_date[1:2], ins=parameterset[1:10,], proc_date, mc.cores = 2)})
 
-outs[order(absolute_profit,decreasing = T)][1:10,]  
-outs[order(absolute_profit,decreasing = T)][1,]  %>% totalReturns(fulldat[stock=='F'], summary = F)
-lm(absolute_profit~short_range+mid_range+buy_trigger+sell_trigger+deathcross+profit,x)%>%summary
+with(results, tapply(absolute_profit, list(short_range, mid_range), mean))
+with(results, tapply(absolute_profit, list(buy_trigger, sell_trigger), mean))
+lm(absolute_profit~short_range+mid_range+buy_trigger+sell_trigger,results)%>%summary
 
-x = parameterset[short_range==5 & mid_range==98 & buy_trigger==0 & 
-                           sell_trigger==0 & !deathcross] %>%
-  crossoverReturns( dat=fulldat, summary = F, date = as.Date('2017-01-01'), transaction_fee=.0001)
-x[,.(returns = abs(cumprod((Sell-Buy)*AdjCloseFilled))[.N],
-     days_held = cumsum((Sell-Buy)/abs(Sell-Buy)*as.integer(Date))[.N] )
-  ,stock][,.(mean(returns),mean(days_held))]
+x = data.table(short_range=3, mid_range=14, long_range=150, buy_trigger=c(0), sell_trigger=c(.2), 
+               cooloff=100, sell_after=365) %>%
+  crossoverReturns(dat=fulldat[stock %in% target_companies[1:150]], summary = F, date = as.Date('2016-06-04'), 
+        end_date = as.Date('2018-06-01'), transaction_fee=.0001)
+x[BuySell!=0 & sample=='test',
+       .(returns = abs(cumprod(BuySell*AdjCloseFilled))[.N],
+         days_held = cumsum(BuySell/abs(BuySell)*as.integer(Date))[.N],
+         purchases = .N/2)
+       ,stock][order(returns)][,.(PctPos=mean(returns>1),
+                                  AvgStockReturn=median(returns), 
+                                  AvgStockDaysHeld=mean(days_held), 
+                                  DaysHeldPerPurchase = sum(days_held)/sum(purchases),
+                                  TotalPurchases=sum(purchases))]
 
+st = 'TI'
+x[stock==st] %>% with(plot(Date, AdjCloseFilled, type='l'))
+x[stock==st] %>% with(points(Date, mid_range_mean, type='l'))
+x[stock==st & Own] %>% with(points(Date, LastBought, type='p', col='blue'))
+x[stock==st & BuySell<0] %>% with(abline(v=Date, col='blue'))
+x[stock==st & BuySell>0] %>% with(abline(v=Date))
+x[stock==st] %>% View()
+x[sample=='test',sum(Own>0),Date] %>% plot(type='l')
 
+aggregates = x[sample=='test' & Own, .(prop_losing = mean(AdjCloseFilled<LastBought), 
+                                       total = .N ),Date][order(Date), .(Date, prop_losing, total, prop_losing_roll = frollmean(fillna(prop_losing, .5), 365, fill=NA, algo="exact", align="right", na.rm=T))] 
+with(aggregates, plot(Date, prop_losing, type='l'))
+with(aggregates, points(Date, prop_losing_roll, type='l', col='red'))
+abline(h=.5)
+
+x[Date=='2005-10-01' & Own>0,stock]
 
 SPY = data.table(tq_get("SVSPX"))
 lapply(test_date, function(test_start){
@@ -66,5 +81,5 @@ lapply(test_date, function(test_start){
              market = pct_diff(SPY[date %in% (test_start+(362:367)), mean(adjusted,na.rm=T)],
                                SPY[date %in% (test_start+(-2:2)), mean(adjusted,na.rm=T)],
                                of=SPY[date %in% (test_start+(-2:2)), mean(adjusted,na.rm=T)])
-  )})
+  )}) %>% rbindlist
 
