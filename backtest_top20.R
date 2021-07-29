7a30c0777a739c86a790fed15cae62050fc21b26
asinayev <asinayev@gmail.com>
2021-07-29 20:51:28 +0000

    fixes for longer lags

--- backtest_top20.R
+++ backtest_top20.R
@@ -6,9 +6,9 @@ source("crossover_strategy.R", local=T)
 library(tidyquant)
 POLYKEY = Sys.getenv('POLYGONKEY')
 
-parameterset = expand.grid(short_range=c(3,14,28), mid_range=c(56), long_range=c(100,200,400),
-                           buy_trigger=c(0), sell_trigger=c(.2,.25,.3), 
-                           cooloff=c(0,100,365), sell_after=c(365,10000), sell_last_day=c(T)
+parameterset = expand.grid(short_range=c(7,28,49), mid_range=c(56), long_range=c(250,500,720),
+                           buy_trigger=c(0), sell_trigger=c(.1,.15,.2), 
+                           cooloff=c(0,100), sell_after=c(35,150,10000), sell_last_day=c(T)
 ) %>% data.table
 # -.1 for buy and -.15 for sell and 7/98 for 2015 stocks
 
@@ -25,8 +25,8 @@ proc_date = function(date, ins, key, sample_size = nrow(ins)){
   fulldat = target_companies %>% 
     # tq_get %>% data.table %>%
     parallel::mclapply(stock_history,
-                       start_date = date-2*365,
-                       end_date = as.Date('2022-01-01'),
+                       start_date = date-2.1*365,
+                       end_date = date+2.1*365,
                        key = POLYKEY, check_ticker=F,
                        mc.cores = 8) %>% rbindlist %>%
     basic_prep(
@@ -37,7 +37,7 @@ proc_date = function(date, ins, key, sample_size = nrow(ins)){
   outs = ins %>% 
     apply(1, as.list) %>%
     parallel::mclapply(crossoverReturns, dat=fulldat, summary=T, date = date, 
-                      end_date=date+365*2, transaction_fee=.0001, mc.cores = 2) %>%
+                      end_date=date+365*2, start_date=date-365*2, transaction_fee=.0001, mc.cores = 2) %>%
     rbindlist %>%
     cbind(ins)
   outs$Date = date
@@ -45,27 +45,31 @@ proc_date = function(date, ins, key, sample_size = nrow(ins)){
 }
 
 results = list()
-for(date in seq(as.Date('2005-08-01'), 
-                as.Date('2019-08-01'), 365)){
+for(date in test_date){
   date=as.Date(date)
   system.time({ results[[date]]=proc_date(date, parameterset, POLYKEY) })
 }
 results = rbindlist(results)
-results = rbind(results, results1)
+
+results_agg = results[,.(avg_profit = mean(avg_profit), avg_profit_sd = sd(avg_profit),
+           median_profit = mean(median_profit), median_profit_sd = sd(median_profit),
+           avg_days_held = mean(avg_days_held), stocks = mean(stocks), 
+           DaysHeldPerPurchase = mean(DaysHeldPerPurchase), trades = mean(trades)),
+        names(parameterset)]
+results_agg[order(median_profit/(avg_days_held+10*trades))] # in terms of profit per day, long ranges with low sell condition are best
 
 with(results, tapply(median_profit, list(buy_trigger, sell_trigger), mean))
 with(results, tapply(DaysHeldPerPurchase, list(buy_trigger, sell_trigger), mean))
 lm(median_profit~
      as.factor(short_range)+
      as.factor(long_range)+
-     as.factor(buy_trigger)+
      as.factor(sell_trigger)+
      as.factor(cooloff)+
      as.factor(sell_after)+
      as.factor(Date)
      ,
    results)%>%summary
-# The best for earnings seems to be "buy at 0 crossover and at least 25 days after loss, sell at +/- .3 or after 550 days.
+# The best for earnings seems to be "buy at 0 crossover and without a cooldown, sell at +/- .3 or after 550 days.
 # The length of the range doesn't matter for the median, but having a shorter long range appears to improve the average
 # Expected profit over 2 years of 3% per stock plus 4% if selling at .3 after 550 days
 lm(avg_days_held~
@@ -96,10 +100,10 @@ lm(trades~
 # So, overall optimal seems to be buying a 14/200 window at 0 crossover, holding 
 # until a .3 change or 365 days pass, and in case of a loss, not buying for 150 days
 
-x = data.table(short_range=3, mid_range=14, long_range=150, buy_trigger=c(0), sell_trigger=c(.2), 
-               cooloff=0, sell_after=10000, sell_last_day=F) %>%
-  crossoverReturns(dat=fulldat[stock %in% target_companies[1:150]], summary = F, date = as.Date('2015-01-01'), 
-                   end_date = as.Date('2019-01-01'), transaction_fee=.0001)
+x = data.table(short_range=49, mid_range=14, long_range=720, buy_trigger=c(0), sell_trigger=c(.15), 
+               cooloff=100, sell_after=100000, sell_last_day=T) %>%
+  crossoverReturns(dat=fulldat[stock %in% target_companies[1:150]], summary = F, date = date, 
+                   end_date = date+365*2, start_date=date-2*365, transaction_fee=.0001)
 
 x[BuySell!=0 & sample=='test',
   .(returns = abs(cumprod(BuySell*AdjCloseFilled))[.N],
@@ -111,18 +115,22 @@ x[BuySell!=0 & sample=='test',
                              DaysHeldPerPurchase = sum(days_held)/sum(purchases),
                              TotalPurchases=sum(purchases))]
 
-st = 'AAPL'
+st = 'BIIB'
 x[stock==st] %>% with(plot(Date, AdjCloseFilled, type='l'))
 x[stock==st] %>% with(points(Date, mid_range_mean, type='l'))
 x[stock==st & Own] %>% with(points(Date, LastBought, type='p', col='blue'))
 x[stock==st & BuySell<0] %>% with(abline(v=Date, col='blue'))
 x[stock==st & BuySell>0] %>% with(abline(v=Date))
 x[stock==st] %>% View()
-x[sample=='test',sum(Own>0),Date] %>% plot(type='l')
+x[stock==st & BuySell!=0]
 
-aggregates = x[sample=='test' & Own, .(prop_losing = mean(AdjCloseFilled<LastBought), 
-                                       total = .N ),Date][order(Date), .(Date, prop_losing, total, prop_losing_roll = frollmean(fillna(prop_losing, .5), 365, fill=NA, algo="exact", align="right", na.rm=T))] 
+aggregates = x[sample=='test' & Own, .(prop_losing = mean(AdjCloseFilled<LastBought),
+                                       owned = sum(Own>0),
+                                       total = .N ),Date][order(Date)]
+aggregates[,prop_losing_roll := frollmean(fillna(prop_losing, .5), 365, fill=NA, algo="exact", align="right", na.rm=T)] 
+aggregates[,owned:=owned/max(owned)]
 with(aggregates, plot(Date, prop_losing, type='l'))
+with(aggregates, points(Date, owned, type='l', col='gray'))
 with(aggregates, points(Date, prop_losing_roll, type='l', col='red'))
 abline(h=.5)
 
