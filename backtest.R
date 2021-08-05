bfc244dfad32153f264d3d06ef5ddaa642d5bdbe
asinayev <asinayev@gmail.com>
2021-08-05 16:49:42 -0400

    Update backtest.R

--- backtest.R
+++ backtest.R
@@ -1,3 +1,4 @@
+
 setwd("stonksanalysis")
 source("prep_data.R", local=T)
 source("polygon.R", local=T)
@@ -23,7 +24,7 @@ filter_range = function(fulldat, company_dates){
   fulldat = merge(fulldat, validrange, all.x=T, on=c('stock','Date')) 
   fulldat[,valid:=!is.na(valid)]
   fulldat=fulldat[,minValid:=min(ifelse(valid,Date,NA),na.rm=T)-365*2,stock]
-  fulldat=fulldat[,maxValid:=max(ifelse(valid,Date,NA),na.rm=T),stock]
+  fulldat=fulldat[,maxValid:=max(ifelse(valid,Date,NA),na.rm=T)+365,stock]
   fulldat[Date>=minValid & Date<=maxValid,
           .(stock,Date,AdjClose,high,low,volume,AdjCloseFilled,hiFilled,loFilled,atr, valid)]
 }
@@ -57,17 +58,17 @@ backtest_dat = function(dates, key){
 fulldat = backtest_dat(seq(as.Date('2005-08-01'), as.Date('2019-08-01'), 365),
                        POLYKEY)
 
-parameterset = expand.grid(short_range=c(7), long_range=c(500),
-                           buy_trigger=c(-.15), cooloff=c(0), buy_trigger_days = c(17),
-                           sell_hi=c(.275), sell_lo=c(.225), sell_atr = c(10),
-                           sell_days=c(100,120), sell_last=c(T)
+parameterset = expand.grid(short_range=c(7), long_range=c(650),
+                           buy_trigger=c(0,-.175), cooloff=c(400), buy_trigger_days = c(17,300),
+                           sell_hi=c(.275,.2), sell_lo=c(.25,.15), sell_atr = c(3,10,100),
+                           sell_days=c(120), sell_last=c(T)
 )
 
 
 results = parameterset %>% 
   apply(1, as.list) %>%
   parallel::mclapply(crossoverReturns, dat=fulldat, summary_only=T, 
-                     transaction_fee=.0001, mc.cores = cores) %>%
+                     transaction_fee=.0001, mc.cores = 4) %>%
   rbindlist %>%
   cbind(parameterset)
 
@@ -76,35 +77,27 @@ results[order(avg_profit/(days_held_per_purchase+10), decreasing=T)]
 
 
 #Examine a single date
-date = dates[1]
-x = data.table(short_range=7, mid_range=14, long_range=500, 
-               buy_trigger=-.15, buy_trigger_days=17,
-               sell_hi=.275,sell_lo=.225,  
-               cooloff=0, sell_days=120, sell_last=T, sell_atr=10) %>%
+x = data.table(short_range=7, long_range=650, 
+               buy_trigger=-.175, buy_trigger_days=17,
+               sell_hi=.275,sell_lo=.25,  
+               cooloff=400, sell_days=120, sell_last=T, sell_atr=10) %>%
   crossoverReturns(dat=fulldat, summary = F, transaction_fee=.0001)
 
+x[order(sample(nrow(x)))][BuySell>0, .(stock,Date,BuySell*AdjCloseFilled)]
 x[BuySell>0,.(PctPos=mean(BuySell*AdjCloseFilled>1),
-                             AvgStockReturn=median(BuySell*AdjCloseFilled), 
-                             TotalPurchases=.N)]
+              AvgStockReturn=median(BuySell*AdjCloseFilled), 
+              TotalPurchases=.N)]
 
-st = 'JCP'
+st = 'MFG'
 x[stock==st] %>% with(plot(Date, AdjCloseFilled, type='l'))
 x[stock==st] %>% with(points(Date, mid_range_mean, type='l'))
 x[stock==st & Own] %>% with(points(Date, LastBought, type='p', col='blue'))
 x[stock==st & BuySell<0] %>% with(abline(v=Date, col='blue'))
 x[stock==st & BuySell>0] %>% with(abline(v=Date))
-x[stock==st] %>% View()
 x[stock==st & BuySell!=0]
+x[stock==st] %>% View()
 
-aggregates = x[sample=='test' & Own, .(prop_losing = mean(AdjCloseFilled<LastBought),
-                                       owned = sum(Own>0),
-                                       total = .N ),Date][order(Date)]
-aggregates[,prop_losing_roll := frollmean(fillna(prop_losing, .5), 365, fill=NA, algo="exact", align="right", na.rm=T)] 
-aggregates[,owned:=owned/max(owned)]
-with(aggregates, plot(Date, prop_losing, type='l'))
-with(aggregates, points(Date, owned, type='l', col='gray'))
-with(aggregates, points(Date, prop_losing_roll, type='l', col='red'))
-abline(h=.5)
+x[BuySell>0,.(avg_profit = mean(BuySell*AdjCloseFilled), sales=.N),year(Date)][order(year)]
 
 x[Date=='2005-10-01' & Own>0,stock]
 
