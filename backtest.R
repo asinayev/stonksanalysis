2506068276b75d31c072fe14b57c61553d7c0db8
asinayev <asinayev@gmail.com>
2021-08-10 01:58:43 +0000

    add new parameters to testing

--- backtest.R
+++ backtest.R
@@ -7,7 +7,7 @@ source("crossover_strategy.R", local=T)
 library(tidyquant)
 POLYKEY = Sys.getenv('POLYGONKEY')
 cores = 4
-poly_cores=min(cores*8,48)
+poly_cores=min(cores*4)
 
 get_financials = function(date, key){
   print(paste('getting date', date))
@@ -58,10 +58,10 @@ backtest_dat = function(dates, key){
 fulldat = backtest_dat(seq(as.Date('2005-08-01'), as.Date('2019-08-01'), 365),
                        POLYKEY)
 gc()
-parameterset = expand.grid(short_range=c(3,7,49,100,150), long_range=c(250,350,500,650),
-                           buy_trigger=c(0,-.1,-.15), cooloff=c(100,400), buy_trigger_days = c(14,21,35,300),
-                           sell_hi=c(.3,.15), sell_lo=c(.15,.3), sell_atr = c(7,10,15,100),
-                           sell_days=c(70,120,365,365*2), sell_last=c(T)
+parameterset = expand.grid(short_range=c(49,75,98), long_range=c(250,300,350),
+                           buy_trigger=c(-.1), cooloff=c(400), buy_trigger_days_max = c(100), buy_trigger_days_min = c(28),
+                           sell_hi=c(.25), sell_lo=c(.25), sell_atr = c(100),
+                           sell_days=c(365), sell_last=c(T)
 )
 
 
@@ -74,32 +74,29 @@ results = parameterset %>%
 
 results[order(avg_profit/(days_held_per_purchase+30), decreasing=T)]
 
-
-
 #Examine a single date
-x = data.table(short_range=7, long_range=650, 
-               buy_trigger=-.175, buy_trigger_days=17,
-               sell_hi=.275,sell_lo=.25,  
-               cooloff=400, sell_days=120, sell_last=T, sell_atr=10) %>%
+x = data.table(short_range=75, long_range=300, 
+               buy_trigger=-.1, cooloff=400, buy_trigger_days_max=100, buy_trigger_days_min=28,
+               sell_hi=.25,sell_lo=.25,  
+               sell_atr=100, sell_days=365, sell_last=T) %>%
   crossoverReturns(dat=fulldat, summary = F, transaction_fee=.0001)
 
-x[order(sample(nrow(x)))][BuySell>0, .(stock,Date,BuySell*AdjCloseFilled)]
+x[order(sample(nrow(x)))][BuySell>0, .(stock,Date,BuySell*AdjCloseFilled)][order(V3)]
 x[BuySell>0,.(PctPos=mean(BuySell*AdjCloseFilled>1),
               AvgStockReturn=median(BuySell*AdjCloseFilled), 
               TotalPurchases=.N)]
 
-st = 'MFG'
+st = 'MMM'
 x[stock==st] %>% with(plot(Date, AdjCloseFilled, type='l'))
-x[stock==st] %>% with(points(Date, mid_range_mean, type='l'))
 x[stock==st & Own] %>% with(points(Date, LastBought, type='p', col='blue'))
 x[stock==st & BuySell<0] %>% with(abline(v=Date, col='blue'))
 x[stock==st & BuySell>0] %>% with(abline(v=Date))
 x[stock==st & BuySell!=0]
 x[stock==st] %>% View()
 
-x[BuySell>0,.(avg_profit = mean(BuySell*AdjCloseFilled), sales=.N),year(Date)][order(year)]
-
-x[Date=='2005-10-01' & Own>0,stock]
+x[BuySell>0,.(avg_profit = mean(BuySell*AdjCloseFilled), sales=.N),year(Date)][order(year)] %>% with(plot(x=year, y=avg_profit, cex=sales/10))
+abline(h=1)
+x[BuySell>0,.(avg_profit = mean(BuySell*AdjCloseFilled), sales=.N),year(Date)==2009]
 
 SPY = data.table(tq_get("SVSPX"))
 lapply(test_date, function(test_start){
