b87ae2e342833068e906aa65e374effd16aec75d
asinayev <asinayev@gmail.com>
2021-08-19 19:55:03 +0000

    starting to work on all crossovers

---
+++ research/all_crossovers_data.R
@@ -0,0 +1,79 @@
+source('crossover_strategy.R')
+
+buySellResearch = function(los, his, closes, crosslong, atr, rsi, valid, buysell_pars, n){
+  
+  crossovers=data.table(i_bought=c(0), buy_price=0, sell_price=0, rsi=0, 
+                        rel_atr=0, maxDip=0, daysCrossed=0)
+  
+  daysCrossed = 0
+  maxDip = 0
+  
+  if(any(!is.na(atr))){
+    maxValidi = max(which(!is.na(atr)))
+    for (i in 2:maxValidi){
+      # increment the counters
+      if(!is.na(crosslong[i-1]) && crosslong[i-1]<buysell_pars$buy_trigger){
+        daysCrossed = daysCrossed + 1
+        maxDip = max(maxDip, buysell_pars$buy_trigger-crosslong[i-1])
+      } else { 
+        daysCrossed=maxDip=0 
+      }
+      if(i==maxValidi){ # on the last day
+        if(buysell_pars$sell_last){
+          crossovers[is.na(sell_price)]$sell_price=closes[i]
+        }
+        next # and then quit
+      }
+      
+      if(is.na(rsi[i]) || is.na(atr[i]) || is.na(crosslong[i-1]) || is.na(crosslong[i]) ){
+        next # skip if necessary values are missing
+      }
+      
+      if(crosslong[i-1]<buysell_pars$buy_trigger &&  # and the crossover was lower than cutoff yesterday
+         crosslong[i]>buysell_pars$buy_trigger && # but is higher than cutoff today
+         valid[i]){ # Buy if in the valid period){  # and the RSI is low enough
+        crossovers = rbind(crossovers,
+                           data.table(i_bought=i, buy_price=closes[i], sell_price=NA, 
+                                      rsi=rsi[i], rel_atr=atr[i]/closes[i], maxDip, daysCrossed) ) 
+        }
+  
+      periodMaxPrice = lapply(crossovers[is.na(sell_price)]$i_bought, 
+                              function(i_bought){max(closes[i_bought:i])} ) %>% unlist
+      crossovers[is.na(sell_price) & (
+        (his[i]>buy_price*(1+buysell_pars$sell_hi)) |
+        ((i-i_bought)>buysell_pars$sell_days) |
+        (los[i]<pmax(periodMaxPrice*(1-buysell_pars$sell_lo), 
+                    periodMaxPrice-buysell_pars$sell_atr*atr[i]) 
+         ) 
+        ), sell_price:=closes[i]]
+    
+    }
+  }
+  return(crossovers)
+}
+
+
+crossover_research=function(pars=list(), dat){
+  pars=as.list(pars)
+  required_pars = c("short_range",      "long_range",               'crossover_units',
+                    "buy_trigger",      #"min_dip",                  "buy_trigger_days_max",     "buy_trigger_days_min",  
+                    "sell_hi",          "sell_lo",                  "sell_atr",         
+                    "sell_days",        "sell_last")
+  (required_pars %in% names(pars)) %>% all %>% stopifnot
+  
+  dat = crossover_prep(dat, short_range = pars$short_range,long_range = pars$long_range, crossover_units = pars$crossover_units)
+  dat[,c('i_bought', 'buy_price', 'sell_price', 'rsi', 
+         'rel_atr', 'maxDip', 'daysCrossed') := buySellResearch(loFilled, hiFilled, AdjCloseFilled, CrossoverLong, atr, rsi, valid, pars, .N),
+      .(stock)] 
+  dat
+}
+
+parameterset = expand.grid(short_range=c(50), long_range=c(250), crossover_units=c(''),
+                           buy_trigger=c(-.1),
+                           buy_atr_min=c(0.02), buy_rsi_max=c(.5),
+                           sell_hi=c(.15), sell_lo=c(.25), sell_atr = c(100),
+                           sell_days=c(180), sell_last=c(T)
+)
+
+crossover_research(pars = parameterset, fulldat )
+
