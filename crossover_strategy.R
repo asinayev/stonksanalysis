ce3bb5c0739fdb30e40d5385d732fd46a4721c43
asinayev <asinayev@gmail.com>
2021-08-19 14:49:36 +0000

    simplify the buysell rules a bit

--- crossover_strategy.R
+++ crossover_strategy.R
@@ -20,70 +20,61 @@ crossover_prep = function(indat,
 buySellSeq = function(los, his, closes, crosslong, atr, rsi, valid, buysell_pars, n){
   
   shares_sold = rep(0,n) #or negative when bought
-  
-  maxValidi = max(which(!is.na(atr)))
-  if(maxValidi<0){
-    return(shares_sold)
-  }
-  
-  lastBoughtPrice = buySignal = periodMaxPrice = -1
+  lastBoughtPrice = periodMaxPrice = -1
   daysSincePurchase = buysell_pars$sell_days
   daysCrossed = 0
   maxDip = 0
   
-  for (i in 2:maxValidi){
-    # increment the counters
-    daysSincePurchase = daysSincePurchase + 1
-    buySignal = buySignal - 1
-    if(!is.na(crosslong[i-1]) && crosslong[i-1]<buysell_pars$buy_trigger){
-      daysCrossed = daysCrossed + 1
-      maxDip = max(maxDip, buysell_pars$buy_trigger-crosslong[i-1])
-    } else { 
-      daysCrossed=maxDip=0 
-    }
-    if(i==maxValidi){ # on the last day
-      if(lastBoughtPrice>0 && buysell_pars$sell_last){
-        shares_sold[i]= 1/lastBoughtPrice   # sell if you own
+  if(any(!is.na(atr))){
+    maxValidi = max(which(!is.na(atr)))
+    for (i in 2:maxValidi){
+      # increment the counters
+      daysSincePurchase = daysSincePurchase + 1
+      if(!is.na(crosslong[i-1]) && crosslong[i-1]<buysell_pars$buy_trigger){
+        daysCrossed = daysCrossed + 1
+        maxDip = max(maxDip, buysell_pars$buy_trigger-crosslong[i-1])
+      } else { 
+        daysCrossed=maxDip=0 
       }
-      next # and then quit
-    }
-    
-    if(is.na(rsi[i]) || is.na(atr[i]) || is.na(crosslong[i-1]) || is.na(crosslong[i]) ){
-      next # skip if necessary values are missing
-    }
-    
-    if(crosslong[i-1]<buysell_pars$buy_trigger &&  # and the crossover was lower than cutoff yesterday
-       crosslong[i]>buysell_pars$buy_trigger && # but is higher than cutoff today
-       daysCrossed>buysell_pars$buy_trigger_days_min && # and the lines have been crossed long enough
-       daysCrossed<buysell_pars$buy_trigger_days_max # but not too long
-    ){
-      buySignal=1
-    }
-    if(buySignal>0 &&
-       valid[i] && # Buy if in the valid period
-       lastBoughtPrice==-1 && # and not already holding
-       maxDip>buysell_pars$min_dip && # and the max dip is big enough
-       atr[i]/closes[i]>buysell_pars$buy_atr_min && # and the ATR is high enough
-       rsi[i]<buysell_pars$buy_rsi_max){  # and the RSI is low enough
-      lastBoughtPrice = periodMaxPrice = closes[i]
-      shares_sold[i]= -1/lastBoughtPrice
-      daysSincePurchase=0
-    } else if (lastBoughtPrice>0){ # When you own
-      periodMaxPrice = max(periodMaxPrice, closes[i])
-      if(his[i]>lastBoughtPrice*(1+buysell_pars$sell_hi)){ #And the high is high enough
-        shares_sold[i]= 1/lastBoughtPrice
-        lastBoughtPrice = periodMaxPrice = -1
-      } else if (daysSincePurchase>buysell_pars$sell_days){ # Or you held long enough
-        shares_sold[i]= 1/lastBoughtPrice
-        lastBoughtPrice = periodMaxPrice = -1
-      } else if (los[i]<max(periodMaxPrice*(1-buysell_pars$sell_lo), 
-                            periodMaxPrice-buysell_pars$sell_atr*atr[i]) ){ #Or low is low enough
-        shares_sold[i]= 1/lastBoughtPrice
-        lastBoughtPrice = periodMaxPrice = -1
+      if(i==maxValidi){ # on the last day
+        if(lastBoughtPrice>0 && buysell_pars$sell_last){
+          shares_sold[i]= 1/lastBoughtPrice   # sell if you own
+        }
+        next # and then quit
+      }
+      
+      if(is.na(rsi[i]) || is.na(atr[i]) || is.na(crosslong[i-1]) || is.na(crosslong[i]) ){
+        next # skip if necessary values are missing
+      }
+      
+      if(crosslong[i-1]<buysell_pars$buy_trigger &&  # and the crossover was lower than cutoff yesterday
+         crosslong[i]>buysell_pars$buy_trigger && # but is higher than cutoff today
+         daysCrossed>buysell_pars$buy_trigger_days_min && # and the lines have been crossed long enough
+         daysCrossed<buysell_pars$buy_trigger_days_max && # but not too long
+         valid[i] && # Buy if in the valid period
+         lastBoughtPrice==-1 && # and not already holding
+         maxDip>buysell_pars$min_dip && # and the max dip is big enough
+         atr[i]/closes[i]>buysell_pars$buy_atr_min && # and the ATR is high enough
+         rsi[i]<buysell_pars$buy_rsi_max){  # and the RSI is low enough
+        lastBoughtPrice = periodMaxPrice = closes[i]
+        shares_sold[i]= -1/lastBoughtPrice
+        daysSincePurchase=0
+      } else if (lastBoughtPrice>0){ # When you own
+        periodMaxPrice = max(periodMaxPrice, closes[i])
+        if(his[i]>lastBoughtPrice*(1+buysell_pars$sell_hi)){ #And the high is high enough
+          shares_sold[i]= 1/lastBoughtPrice
+          lastBoughtPrice = periodMaxPrice = -1
+        } else if (daysSincePurchase>buysell_pars$sell_days){ # Or you held long enough
+          shares_sold[i]= 1/lastBoughtPrice
+          lastBoughtPrice = periodMaxPrice = -1
+        } else if (los[i]<max(periodMaxPrice*(1-buysell_pars$sell_lo), 
+                              periodMaxPrice-buysell_pars$sell_atr*atr[i]) ){ #Or low is low enough
+          shares_sold[i]= 1/lastBoughtPrice
+          lastBoughtPrice = periodMaxPrice = -1
+        }
       }
     }
   }
-  
   return(shares_sold)
 }
 
