a9de35401e5b1a36d60477e5348ad50771403eb6
asinayev <asinayev@gmail.com>
2021-11-07 03:44:34 +0000

    adding fundamentals to analysis

--- other_datasources/analyzing_event_data.R
+++ other_datasources/analyzing_event_data.R
@@ -1,9 +1,10 @@
 require(data.table)
 require(tidyquant)
 
-key = ''
-share_buybacks = jsonlite::fromJSON(txt='https://app.endpoints.levelfields.ai/scenarios.php?scenario_id=164&key=  &start_date=2015-11-05&end_date=2021-11-05&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJMRVZFTEZJRUxEUyIsImlhdCI6MTYzNjEzMjgxNCwibmJmIjoxNjM2MTMyODE0LCJleHAiOjE2MzYxNjg4MTQsImRhdGEiOnsiZmlyc3RuYW1lIjoiQWxla3NhbmRyIiwibGFzdG5hbWUiOiJTaW5heWV2IiwiZW1haWwiOiJhc2luYXlldkBnbWFpbC5jb20iLCJyb2xlIjoiVVNFUiIsInJlY3VybHlfdXNlcl9hY2NvdW50X2lkIjoiQWxla3NhbmRyU2luYXlldi02NjdhOGVjZjhkNTRhZWI4NDU4NiIsInJlY3VybHlfcGxhbl9pZCI6ImJldGEwNTkiLCJyZWN1cmx5X3N1YnNjcmlwdGlvbl9pZCI6InB0aDY2OHJ2bGk0cyIsInJlY3VybHlfc3Vic2NyaXB0aW9uX2VuZF9kYXRlIjoiMjAyMS0xMi0wMiAxODo0NToyOSIsImZpc3J0X2xvZ2luIjpmYWxzZSwiaXNfY2FuY2VsbGVkIjpmYWxzZX19.P7PV3lRCiVhpb49AL5koU32v1mU2FFj4-7bGbMt4chKv_l8eMhRYW9gTZIJMFNXh1HdITAXQXTBjQ-2egDRwgA')
-dividend_creations = jsonlite::fromJSON(txt='https://app.endpoints.levelfields.ai/scenarios.php?scenario_id=168&key=  &start_date=2015-11-05&end_date=2021-11-05&jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJMRVZFTEZJRUxEUyIsImlhdCI6MTYzNjEzMjgxNCwibmJmIjoxNjM2MTMyODE0LCJleHAiOjE2MzYxNjg4MTQsImRhdGEiOnsiZmlyc3RuYW1lIjoiQWxla3NhbmRyIiwibGFzdG5hbWUiOiJTaW5heWV2IiwiZW1haWwiOiJhc2luYXlldkBnbWFpbC5jb20iLCJyb2xlIjoiVVNFUiIsInJlY3VybHlfdXNlcl9hY2NvdW50X2lkIjoiQWxla3NhbmRyU2luYXlldi02NjdhOGVjZjhkNTRhZWI4NDU4NiIsInJlY3VybHlfcGxhbl9pZCI6ImJldGEwNTkiLCJyZWN1cmx5X3N1YnNjcmlwdGlvbl9pZCI6InB0aDY2OHJ2bGk0cyIsInJlY3VybHlfc3Vic2NyaXB0aW9uX2VuZF9kYXRlIjoiMjAyMS0xMi0wMiAxODo0NToyOSIsImZpc3J0X2xvZ2luIjpmYWxzZSwiaXNfY2FuY2VsbGVkIjpmYWxzZX19.P7PV3lRCiVhpb49AL5koU32v1mU2FFj4-7bGbMt4chKv_l8eMhRYW9gTZIJMFNXh1HdITAXQXTBjQ-2egDRwgA')
+fundamentals = fread("~/stonksanalysis/other_datasources/nasdaq_screener_1636253557582.csv") #from https://www.nasdaq.com/market-activity/stocks/screener
+
+share_buybacks = jsonlite::fromJSON(txt='')
+dividend_creations = jsonlite::fromJSON(txt='')
 
 share_buybacks = data.frame(share_buybacks[c('symbol','event_date_est')])
 share_buybacks$eventtype='buyback'
@@ -12,15 +13,15 @@ dividend_creations$eventtype='dividient_creation'
 eventsdf = rbind(share_buybacks,dividend_creations)
 
 eventsdf$event_date_est=lubridate::ymd_hms(eventsdf$event_date_est, tz = "EST")
-eventsdf$is_after_hours = lubridate::hour(eventsdf$event_date_est)>15
+eventsdf$is_after_hours = lubridate::hour(eventsdf$event_date_est)>9
 eventsdf$during_trading = lubridate::hour(eventsdf$event_date_est)<16 & lubridate::hour(eventsdf$event_date_est)>9
-eventsdf$event_date = as.Date(eventsdf$event_date_est)
+eventsdf$event_date = as.Date(eventsdf$event_date_est, tz = "EST")
 
 
 system.time(stockdat <- parallel::mclapply(unique(eventsdf$symbol),
                               tq_get,
-                              from=min(eventsdf$next_trade_date, na.rm=T),
-                              to=max(eventsdf$next_trade_date, na.rm=T),
+                              from=min(eventsdf$event_date, na.rm=T),
+                              to=max(eventsdf$event_date, na.rm=T)+3,
                               mc.cores = 8))
 stockdat = rbindlist(stockdat[lapply(stockdat, is.data.frame)%>%unlist])
 setorder(stockdat, symbol, date)
@@ -39,15 +40,18 @@ events_prices = merge(data.table(eventsdf),
                        by.x = c('symbol', "event_date"),
                        by.y = c('symbol','date'), 
                        all.x=T )
+events_prices = merge(events_prices,
+                      fundamentals[,c('Symbol','Market Cap', 'Country', 'IPO Year', 'Volume', 'Sector', 'Industry')],
+                      by.x = c('symbol'),
+                      by.y = c('Symbol'), 
+                      all.x=T )
 
-events_prices[,gain_day_1:=ifelse( is_after_hours, (nextopen-nextclose)/nextopen )]
-events_prices[,gain_day_2:=ifelse( is_after_hours, (next2open-next2close)/next2open )]
-shares_buyback[,gain_cum:=(close.y-open.x)/open.x]
-shares_buyback[,low_cum:=pmin(low.x,low.y)]
-shares_buyback[,high_cum:=pmax(high.x,high.y)]
+events_prices[,gain_day_1:=ifelse( is_after_hours, (nextclose-nextopen)/nextopen, (close-open)/open )]
+events_prices[,gain_day_2:=ifelse( is_after_hours, (next2close-next2open)/next2open, (nextclose-nextopen)/nextopen )]
+events_prices[,gain_cum:=ifelse( is_after_hours, (next2close-nextopen)/nextopen, (nextclose-open)/open )]
 
-shares_buyback$gain_cum%>%summary
-shares_buyback[,gain_cum:=ifelse( (high_cum - open.x)/open.x > .1, 
-                                  .1,
-                                  gain_cum)]
-shares_buyback$gain_cum%>%summary
+events_prices$gain_cum%>%summary
+
+events_prices[,.(mean(gain_cum,na.rm=T), median(gain_cum, na.rm=T), .N),.(`Market Cap`>25*10^9, eventtype)]
+events_prices[,.(mean(gain_cum,na.rm=T), median(gain_cum, na.rm=T), .N),.(Sector,eventtype)][order(N)]
+events_prices[,.(mean(gain_cum,na.rm=T), median(gain_cum, na.rm=T), .N),.(is_after_hours,eventtype)][order(N)]
