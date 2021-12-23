96de985538cb48eba4e38ad86601faddce8e6b8f
asinayev <asinayev@gmail.com>
2021-12-23 23:51:04 +0000

    working on other day strategies

--- research/foreign_stocks.R
+++ research/foreign_stocks.R
@@ -19,32 +19,38 @@ prices <- fundamentals$Symbol %>%
 )
 
 prices = prices[symbol %in% prices[!is.na(close) & !is.na(open),.N,symbol][N>365, symbol]]
-prices = prices[order(symbol,date)]
-prices[,c("lag1close", "lag2close"):=shift(close, n = 1:2, type = "lag")]
-prices[,"lag1open":=shift(open, n = 1, type = "lag")]
-prices[,abroad_delta:= lag1open/lag2close]
-prices[,us_delta:= lag1close/lag1open]
-prices[,day_premarket:= open/lag1close]
+setorder(prices, symbol, date)
+prices[,c("lag1close", "lag2close"):=shift(close, n = 1:2, type = "lag"),symbol]
+prices[,c("lag1open",  "lag2open" ):=shift(open,  n = 1:2, type = "lag"),symbol]
+prices[,c("lag1high",  "lag2high" ):=shift(high,  n = 1:2, type = "lag"),symbol]
+prices[,c("lag1low",   "lag2low"  ):=shift(low,   n = 1:2, type = "lag"),symbol]
 prices[,day_delta:= close/open]
-prices[!is.na(volume),volume_avg:= SMA(shift(volume,1,type='lag'), n = 30, ) ]
+prices[,night_delta:= open/lag1close]
+prices[!is.na(volume),volume_avg:= SMA(shift(volume,1,type='lag'), n = 30, ),symbol ]
+prices[,c("lag1_day_delta",    "lag2_day_delta"   ):=shift(day_delta,    n = 1:2, type = "lag"),symbol]
+prices[,c("lag1_night_delta",  "lag2_night_delta" ):=shift(night_delta,  n = 1:2, type = "lag"),symbol]
 
-prices[!is.na(us_delta) & !is.na(abroad_delta),
+prices[!is.na(lag1_day_delta) & !is.na(lag1_night_delta),
        lagging_corr:=
-         runCor( us_delta, abroad_delta, 365),
+         runCor( lag1_day_delta, lag1_night_delta, 364),
        symbol]
 
-prices_metadata = merge(prices,fundamentals,by.x = 'symbol', 
-                        by.y = 'Symbol',all.x = T)
 
 # Determine correct range to use for volume (it changes over time)
-# plot_data = prices_metadata[, .(out = mean(day_delta,na.rm=T), .N),
-#                             .(day_premarket = round(day_premarket,2),
+# plot_data = prices_metadata[, .(out = min(1.1, mean(day_delta,na.rm=T)), .N),
+#                             .(pred_change = round(night_delta,2),
 #                               lagging_corr = round(lagging_corr,1),
 #                               year(date),
 #                               volume = round(log(volume_avg+1)))
 #                             ][N>30 & volume>8]
-#  
-# ggplot(plot_data[year>2018 & volume %between% c(7,15)], aes(x=day_premarket,
+# plot_data = prices_metadata[, .(out = min(1.1, mean(day_delta,na.rm=T)), .N),
+#                             .(pred_change = round(lag_day_delta,2),
+#                               lagging_corr = round(lagging_corr2,1),
+#                               year(date),
+#                               volume = round(log(volume_avg+1)))
+# ][N>30 & volume>8]
+# 
+# ggplot(plot_data[year>2015 & volume %between% c(7,15)], aes(x=pred_change,
 #                       y=lagging_corr, color=out, size=N))+
 #   geom_point(alpha=.9) +
 #   scale_color_gradient2(midpoint = 1, low = "red", mid ="white", high = "blue", space = "Lab" )+
@@ -60,12 +66,12 @@ prices_metadata = merge(prices,fundamentals,by.x = 'symbol',
 # prices_metadata[log(volume_avg+1)<12 & year(date)>2019,.(mean(lagging_corr,na.rm=T),.N),Sector][order(V1)]
 # Whitelist: Consumer Durables, Consumer Non-Durables, Consumer Services, Technology and recently, Finance
 
-# prices_metadata[log(volume_avg+1)<11 & Sector %in% c('Consumer Durables', 'Consumer Non-Durables','Consumer Services', 'Technology', 'Finance'),.(mean(abs(lagging_corr2),na.rm=T),.N),Country][order(V1)]
+# prices_metadata[log(volume_avg+1)<11 & Sector %in% c('Consumer Durables', 'Consumer Non-Durables','Consumer Services', 'Technology', 'Finance'),
+#                 .(mean(abs(lagging_corr2),na.rm=T),.N),Country][order(V1)]
 
-subsample = prices_metadata[
-  log(volume_avg+1) %between% c(10,13)]
+subsample = prices[log(volume_avg+1) %between% c(10,13)]
 
-subsample[day_premarket<.97 & lagging_corr< -.4,
+subsample[night_delta<.97 & lagging_corr< -.4,
           .(mean(day_delta,na.rm=T),.N), year(date)][order(year)]
 subsample[date==max(date, na.rm=T) & lagging_corr< -.4,
           .(date, symbol, close, buy = trunc(close*97,3)/100 , sell = (trunc(close*103,3)+1)/100)] %>%
