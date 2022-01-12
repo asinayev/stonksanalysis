fbea7fb2bb88330e1fefb5430d6766bd72c91dc4
asinayev <asinayev@gmail.com>
2022-01-12 10:58:37 -0500

    add options to launch

--- launch.sh
+++ launch.sh
@@ -1,4 +1,10 @@
-Rscript /home/repos/stonksanalysis/apply_news.R /home/repos/stonksanalysis && \
-Rscript /home/repos/stonksanalysis/implement_foreign/get_data.R && \
-Rscript /home/repos/stonksanalysis/implement_foreign/regression_outs.R && \
-Rscript /home/repos/stonksanalysis/implement_foreign/respond_overnight.R
+if [ $run_what == "all" || $run_what == "news" ] ; then
+	Rscript /home/repos/stonksanalysis/apply_news.R /home/repos/stonksanalysis
+fi
+if [ $run_what == "all" || $run_what == "data" ] ; then
+	Rscript /home/repos/stonksanalysis/implement_foreign/get_data.R
+fi
+if [ $run_what == "all" || $run_what == "pred" ] ; then
+	Rscript /home/repos/stonksanalysis/implement_foreign/regression_outs.R && \
+	Rscript /home/repos/stonksanalysis/implement_foreign/respond_overnight.R
+fi
