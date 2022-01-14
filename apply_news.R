19e4d34dd1fb33222383e6829f778ee253c3d89e
asinayev <asinayev@gmail.com>
2022-01-14 09:30:25 -0500

    change column names to fit with submittal criteria

--- apply_news.R
+++ apply_news.R
@@ -65,17 +65,18 @@ matching_news(current_news, keyword=c('Health', 'Partnerships', 'Press releases'
               publisher='GlobeNewswire Inc.', max_tickers=Inf) %>%
   enrich(current_moves, POLYKEY) %>%
   subset(log(market_cap)<21, select=c('symbol','price','prevDay.c','volume')) %>%
+  dplyr::mutate( close=PrevDay.c, buy=0, sell=round(prevDay.c*1.015,2)) %>%
   fwrite('/tmp/healthshort.csv')
 # Short PennyStocks' penny stocks with upward movement
 matching_news(current_news, keyword=NA, publisher='PennyStocks', max_tickers=Inf) %>%
   enrich(current_moves, POLYKEY) %>%
   subset(log(market_cap)<21, select=c('symbol','prevDay.c','volume')) %>%
-  dplyr::mutate( buy=0, sell=round(prevDay.c*1.015,2)) %>%
+  dplyr::mutate( close=PrevDay.c, buy=0, sell=round(prevDay.c*1.015,2)) %>%
   fwrite('/tmp/pennyshort.csv')
 # short Benzinga's penny stocks with Penny Stocks and Small Cap keywords and upward movement >1%
 matching_news(current_news, keyword=c('Penny Stocks', 'Small Cap'), publisher='Benzinga', max_tickers=Inf) %>%
   enrich(current_moves, POLYKEY) %>%
   subset(log(market_cap)<21, select=c('symbol','prevDay.c','volume')) %>%
-  dplyr::mutate( buy=0, sell=round(prevDay.c*1.015,2)) %>%
+  dplyr::mutate( close=PrevDay.c, buy=0, sell=round(prevDay.c*1.015,2)) %>%
   fwrite('/tmp/zingashort.csv')
 
