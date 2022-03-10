dedc4e54c92247ff5ed52fcd36d38be1fa6f398a
asinayev <asinayev@gmail.com>
2022-03-10 09:21:04 -0500

    pass directory

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ if [ $run_what == "all" ] || [ $run_what == "news" ] ; then
 	Rscript /home/repos/stonksanalysis/apply_news.R /home/repos/stonksanalysis
 fi
 if [ $run_what == "all" ] || [ $run_what == "data" ] ; then
-	Rscript /home/repos/stonksanalysis/implement_foreign/get_data.R
+	Rscript /home/repos/stonksanalysis/implement_foreign/get_data.R /home/repos/stonksanalysis
 fi
 if [ $run_what == "all" ] || [ $run_what == "pred" ] ; then
 	Rscript /home/repos/stonksanalysis/implement_foreign/regression_outs.R && \
