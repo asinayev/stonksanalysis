08980774239f34e2312ff6d764209cd9e0924654
asinayev <asinayev@gmail.com>
2022-04-29 14:54:48 -0400

    adding directory

--- implement/get_manual.R
+++ implement/get_manual.R
@@ -1,18 +1,10 @@
-args = commandArgs(trailingOnly=TRUE)
-if(length(args)==0){
-  setwd('~/stonksanalysis')
-} else {
-  setwd(args[1]) 
-}
-source("implement/imports.R", local=T)
-
 install.packages("googlesheets4")
 library(googlesheets4)
 
 read_sheet("https://docs.google.com/spreadsheets/d/1bSW8k45QPlnL1aIijoRvRdZYhfKvHoz_TCWVGAZdGaE",
            sheet = 'exceptions') %>%
-  write_strat(strat_name='exceptions', base_dir='/tmp/stonksanalysis')
+  fwrite('/tmp/stonksanalysis/exceptions.csv')
 
 read_sheet("https://docs.google.com/spreadsheets/d/1bSW8k45QPlnL1aIijoRvRdZYhfKvHoz_TCWVGAZdGaE",
            sheet = 'manual_buys') %>%
-  write_strat(strat_name='manual', base_dir='/tmp/stonksanalysis')
+  fwrite('/tmp/stonksanalysis/manual.csv')
--- launch.sh
+++ launch.sh
@@ -5,8 +5,8 @@ if [ $run_what == "am" ] || [ $run_what == "am_data" ] ; then
 	Rscript /home/repos/stonksanalysis/implement/get_data.R /home/repos/stonksanalysis
 fi
 if [ $run_what == "am" ] || [ $run_what == "am_out" ] ; then
-	Rscript /home/repos/stonksanalysis/implement/am_regression.R && \
-	Rscript /home/repos/stonksanalysis/implement/am_stocks.R && \
+	Rscript /home/repos/stonksanalysis/implement/am_regression.R /home/repos/stonksanalysis && \
+	Rscript /home/repos/stonksanalysis/implement/am_stocks.R /home/repos/stonksanalysis && \
 	Rscript /home/repos/stonksanalysis/implement/am_etfs.R /home/repos/stonksanalysis
 fi
 if [ $run_what == "snapshot" ] ; then
