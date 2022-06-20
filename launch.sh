6f6261c0d8c109dc27dbea6f7c62215248afcdb6
asinayev <asinayev@gmail.com>
2022-06-20 15:30:23 +0000

    exclude stocks from PM run

--- launch.sh
+++ launch.sh
@@ -13,6 +13,5 @@ if [ $run_what == "snapshot" ] ; then
 	Rscript /home/repos/stonksanalysis/implement/get_snapshot.R /home/repos/stonksanalysis
 fi
 if [ $run_what == "pm" ] ; then
-	Rscript /home/repos/stonksanalysis/implement/pm_stocks.R /home/repos/stonksanalysis
 	Rscript /home/repos/stonksanalysis/implement/pm_etfs.R /home/repos/stonksanalysis
 fi
