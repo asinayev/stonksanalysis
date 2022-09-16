118cc172104b0de9b8682421526135e70cf14ab1
asinayev <asinayev@gmail.com>
2022-09-16 22:06:21 +0000

    bugfixes

--- implement/imports.R
+++ implement/imports.R
@@ -3,6 +3,7 @@ require(data.table, quietly = T)
 
 source("polygon.R", local=T)
 source("implement/outputs.R", local=T)
+source("implement/features.R", local=T)
 POLYKEY = Sys.getenv('POLYGONKEY')
 out_dir = Sys.getenv('out_dir')
 
