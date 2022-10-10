af8e0a28e2172c011bbb7902ddf4572bf7f1a0d0
asinayev <asinayev@gmail.com>
2022-10-10 14:52:52 +0000

    volatiolity research

--- research/volatility.R
+++ research/volatility.R
@@ -1,5 +1,5 @@
-# Buy tecl, soxl, and sso when vixy spikes 
-spy_prices=prices[symbol%in%c('TECL')]
-spy_prices=merge(spy_prices, prices[symbol=='VIXY',.(date, vix_entry=close/open > 1.015)])
-spy_prices[vix_entry==T]%>%
+# Long tecl, soxl, labu if they fall when vixy spikes 
+spy_prices=prices[symbol%in%c('TECL','SOXL','LABU')]
+spy_prices=merge(spy_prices, prices[symbol=='VIXY',.(date, vix_entry=(close>open*1.005))])
+spy_prices[vix_entry==T & close<open]%>%
   with(performance(date,lead1sellrally/lead1open-1,lead1sellrallydate-date,symbol))
