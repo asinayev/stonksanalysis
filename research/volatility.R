6be75605e8aced37efbd2ff510dbd783c8a48eef
asinayev <asinayev@gmail.com>
2022-10-27 21:05:18 +0000

    better volatility trade

--- research/volatility.R
+++ research/volatility.R
@@ -1,5 +1,6 @@
 # Long tecl, soxl, labu if they fall when vixy rises .5%. Sell when they're up 2.5%
 spy_prices=prices[symbol%in%c('TECL','SOXL','LABU')]
-spy_prices=merge(spy_prices, prices[symbol=='VIXY',.(date, vix_entry=(close>open*1.005))])
-spy_prices[vix_entry==T & close<open]%>%
-  with(performance(date,lead1sellupup/lead1open-1,lead1sellupupdate-date,symbol))
+spy_prices=merge(spy_prices, prices[symbol%in%c('VIXY','VIXM','VXX','VXZ'),
+                                    .(vix_entry=max(close>open*1.0025 )),date],by='date')
+spy_prices[vix_entry==T & close<lag1high & sell_rally_day>1 ]%>%
+  with(performance(date,lead1sell_rally/lead1open-1,lead1sell_rallydate-date,symbol))
