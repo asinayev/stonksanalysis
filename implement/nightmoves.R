6747ceafcdb42d09b54b81128eae92a7060ea2e9
asinayev <asinayev@gmail.com>
2022-11-01 03:14:01 +0000

    remove everything except stocks from nightmoves

--- implement/nightmoves.R
+++ implement/nightmoves.R
@@ -12,7 +12,9 @@ current_moves = "https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tic
   hit_polygon
 current_moves_dt = data.table(current_moves$tickers)
 
-current_moves_dt[todaysChangePerc>15 & min.av>10000 & min.c>5] %>%
+prices = fread('/tmp/prices.csv')
+
+current_moves_dt[todaysChangePerc>15 & min.av>10000 & min.c>5 & ticker %in% prices$symbol] %>%
   dplyr::mutate( symbol=ticker, action='SELL', 
                  strike_price=trunc(prevDay.c*1150,3)/1000, 
                  order_type='LMT', time_in_force='OPG') %>%
