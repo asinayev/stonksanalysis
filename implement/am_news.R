9a742e1a1b512c1cfda30c5dbd3e02628a11858c
asinayev <asinayev@gmail.com>
2022-11-22 09:12:08 -0500

    Fix reference bug

--- implement/am_news.R
+++ implement/am_news.R
@@ -18,12 +18,12 @@ news_moves = just_news %>%
   merge(prices, by=c('date','symbol'), all.x=T)
 
 # long stocks that fell before revenue
-current_news[grepl('earning', title, ignore.case = T) &
-             grepl('revenue', title, ignore.case = T) &
-             publisher.name=='Zacks Investment Research' &
-             avg_volume>50000 & volume>50000 & close>5  & 
-             close/open <.99] %>%
- dplyr::mutate( action='BUY', 
+news_moves[grepl('earning', title, ignore.case = T) &
+           grepl('revenue', title, ignore.case = T) &
+           publisher.name=='Zacks Investment Research' &
+           avg_volume>50000 & volume>50000 & close>5  & 
+           close/open <.99] %>%
+  dplyr::mutate(action='BUY', 
                 order_type='MKT',
                 time_in_force='OPG') %>%
- write_strat(strat_name='zacks_earn')
+  write_strat(strat_name='zacks_earn')
