fd588e45a2449db7953849850308ce3d1b6225bd
asinayev <asinayev@gmail.com>
2023-02-22 13:52:55 -0500

    write stocks with issues properly

--- implement/outputs.R
+++ implement/outputs.R
@@ -1,4 +1,5 @@
 write_strat = function(strat_dat, strat_name, base_dir=out_dir, append=F, col.names=T){
+  strat_dat[,symbol:=gsub("[^[:alnum:] ]", " ", symbol)]
   out_file_name=paste0(base_dir,"/",strat_name,".csv")
   fwrite(strat_dat,out_file_name, append = append, col.names = col.names)
 }
