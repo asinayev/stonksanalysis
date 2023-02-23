d60e30c25df4c77655d1dd707734761dca1aad29
asinayev <asinayev@gmail.com>
2023-02-23 09:06:17 -0500

    Do not put in orders for google

--- implement/outputs.R
+++ implement/outputs.R
@@ -1,5 +1,6 @@
 write_strat = function(strat_dat, strat_name, base_dir=out_dir, append=F, col.names=T){
   strat_dat[,symbol:=gsub("[^[:alnum:] ]", " ", symbol)]
+  strat_dat = strat_dat[!grepl("GOOG", symbol)]
   out_file_name=paste0(base_dir,"/",strat_name,".csv")
   fwrite(strat_dat,out_file_name, append = append, col.names = col.names)
 }
