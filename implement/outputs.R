45798b6128ae9f9a1f2c74b0107e17fb80153c3b
asinayev <asinayev@gmail.com>
2023-03-24 09:33:16 -0400

    increase blacklist

--- implement/outputs.R
+++ implement/outputs.R
@@ -1,6 +1,7 @@
-write_strat = function(strat_dat, strat_name, base_dir=out_dir, append=F, col.names=T){
+write_strat = function(strat_dat, strat_name, base_dir=out_dir, append=F, col.names=T,
+                      blacklist=c('GOOG','GOOGL','VTI','FUTY','VGLT','BAR','VGIT')){
   strat_dat[,symbol:=gsub("[^[:alnum:] ]", " ", symbol)]
-  strat_dat = strat_dat[!grepl("GOOG", symbol)]
+  strat_dat = strat_dat[!symbol %in% blacklist]
   out_file_name=paste0(base_dir,"/",strat_name,".csv")
   fwrite(strat_dat,out_file_name, append = append, col.names = col.names)
 }
