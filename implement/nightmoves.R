52076707c0d9e1144166e83f4574e259b0955338
asinayev <asinayev@gmail.com>
2023-03-26 23:51:39 -0400

    bugfix +

--- implement/nightmoves.R
+++ implement/nightmoves.R
@@ -13,7 +13,7 @@ current_moves = "https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tic
 current_moves_dt = data.table(current_moves$tickers)
 
 fwrite(current_moves_dt[ticker=='SPY'], '/tmp/spy_change.csv') #has todaysChangePerc
-fwrite(current_moves_dt[,.(symbol=ticker, open=prevDay.o, low=prevDay.l, high=prevDay.h, close=prevDay.c, volume=prevDay.v), '/tmp/lastday_prices.csv'])
+fwrite(current_moves_dt[,.(symbol=ticker, open=prevDay.o, low=prevDay.l, high=prevDay.h, close=prevDay.c, volume=prevDay.v)], '/tmp/lastday_prices.csv')
 
 prices = fread('/tmp/prices.csv')
 
