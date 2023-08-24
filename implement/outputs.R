e74c5a5f53a72aeb4b17564a0d61248d0c8b08ea
asinayev <asinayev@gmail.com>
2023-08-24 09:40:43 -0400

    Update outputs.R

--- implement/outputs.R
+++ implement/outputs.R
@@ -3,5 +3,9 @@ write_strat = function(strat_dat, strat_name, base_dir=out_dir, append=F, col.na
   strat_dat[,symbol:=gsub("[^[:alnum:] ]", " ", symbol)]
   strat_dat = strat_dat[!symbol %in% blacklist]
   out_file_name=paste0(base_dir,"/",strat_name,".csv")
-  fwrite(strat_dat,out_file_name, append = append, col.names = col.names)
+  if(! ('strike_price' %in% names(strat_dat))){
+    strat_dat[,strike_price:=close]  
+  }
+  fwrite(strat_dat[,.(symbol,action,order_type,strike_price)],
+         out_file_name, append = append, col.names = col.names)
 }
