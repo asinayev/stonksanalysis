2a844c576a64a91a81c87e5abb930a9c1549e71c
asinayev <asinayev@gmail.com>
2023-08-28 09:30:34 -0400

    time in force is also necessary

--- implement/outputs.R
+++ implement/outputs.R
@@ -6,6 +6,6 @@ write_strat = function(strat_dat, strat_name, base_dir=out_dir, append=F, col.na
   if(! ('strike_price' %in% names(strat_dat))){
     strat_dat[,strike_price:=close]  
   }
-  fwrite(strat_dat[,.(symbol,action,order_type,strike_price)],
+  fwrite(strat_dat[,.(symbol,action,order_type,strike_price,time_in_force)],
          out_file_name, append = append, col.names = col.names)
 }
