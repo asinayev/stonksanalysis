5cafb0265913ebe16baf2c56afa803a473924313
asinayev <asinayev@gmail.com>
2024-04-25 21:48:44 +0000

    trading file fixes for etfs

--- implement/am_etfs.R
+++ implement/am_etfs.R
@@ -37,8 +37,7 @@ prices[,lever:=grepl('2x|3x|leverag|ultra', name, ignore.case = T)]
 prices[order(lever, avg_volume,decreasing = F)][
   date==max(date, na.rm=T) & volume>500000 & close>7 & 
          close<lag1high & sell_rally_day>2 & 
-         avg_delta/sell_rally_avg<.982 ,
-       .(date, symbol, close, volume)] %>%
+         avg_delta/sell_rally_avg<.982] %>%
   dplyr::mutate( action='BUY', order_type='MKT', time_in_force='OPG') %>%
   head(1) %>%
   write_strat(strat_name='rally_etfs')
@@ -46,8 +45,7 @@ prices[order(lever, avg_volume,decreasing = F)][
 prices[order(lever, avg_volume,decreasing=F)][
   date==max(date, na.rm=T) & volume>500000 & close>7 &  
          (((close-low)/avg_range)<.15 ) & 
-         ((high/close) > 1.075 | avg_delta<.99),
-       .(date, symbol, close, volume)]%>%
+         ((high/close) > 1.075 | avg_delta<.99)]%>%
   head(1) %>%
   dplyr::mutate( action='BUY', order_type='MKT', time_in_force='OPG') %>%
   write_strat(strat_name='revert_etfs')
@@ -55,8 +53,7 @@ prices[order(lever, avg_volume,decreasing=F)][
 prices[order(lever, avg_volume, decreasing = F)][
   date==max(date, na.rm=T) & 
     volume>500000 & close>7 & 
-    avg_delta_short<.975 & lagging_corr_long> .35,
-  .(date, symbol, close, volume)] %>%
+    avg_delta_short<.975 & lagging_corr_long> .35] %>%
   head(1) %>%
   dplyr::mutate( action='BUY', order_type='MKT', time_in_force='OPG') %>%
   write_strat(strat_name='corr_long_etfs')
