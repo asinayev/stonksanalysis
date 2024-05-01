a28b6d2107e55803146d8dc338bcb59571c5e744
asinayev <asinayev@gmail.com>
2024-05-01 08:17:33 -0400

    Remove nightbot strategy

--- implement/nightmoves.R
+++ implement/nightmoves.R
@@ -15,9 +15,3 @@ current_moves_dt = data.table(current_moves$tickers)
 fwrite(current_moves_dt[ticker=='SPY'], '/tmp/spy_change.csv') #has todaysChangePerc
 
 prices = fread('/tmp/prices.csv')
-
-current_moves_dt[todaysChangePerc>15 & prevDay.v>1000000 & min.av>10000 & min.c>5 & ticker %in% prices$symbol] %>%
-  dplyr::mutate( symbol=ticker, action='SELL', 
-                 strike_price=trunc(prevDay.c*1150,3)/1000, 
-                 order_type='LMT', time_in_force='OPG') %>%
-  write_strat(strat_name='nightbot')
