3585d8fd5646bf2153df970e791b38ff09e80f3a
asinayev <asinayev@gmail.com>
2024-08-13 15:51:22 +0000

    work adding api calling

--- Dockerfile
+++ Dockerfile
@@ -12,5 +12,12 @@ RUN install2.r --error \
 RUN git clone https://github.com/asinayev/stonksanalysis.git && \
     cd stonksanalysis && \
     git checkout ab914b30249414dcd382add833a58b035d9b04cb 
- 
+
+RUN apt-get update && apt-get install -y \
+    python3-pip
+
+RUN pip3 install --upgrade \
+    google-api-python-client\ 
+    google-generativeai
+
 CMD bash /home/repos/stonksanalysis/launch.sh
--- implement/call_ai.py
+++ implement/call_ai.py
@@ -1,8 +1,20 @@
+from googleapiclient.discovery import build
 import google.generativeai as genai
 import os
 
+my_cse_id = os.environ["SEARCHID"]
 genai.configure(api_key=os.environ["GOOGLEKEY"])
-
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
+
+def google_search(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=os.environ["GOOGLEKEY"])
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
+
+results = google_search(
+    'stackoverflow site:en.wikipedia.org', my_api_key, my_cse_id, num=10, dateRestrict='d1')
+
+def gemini_call()
+
 response = model.generate_content("The opposite of hot is")
 print(response.text)
