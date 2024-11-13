8fb1569223d58b0b68a4837ffe6a3578bd82d78a
asinayev <asinayev@gmail.com>
2024-11-13 16:37:33 -0500

    update prompts

--- ai_news/buybacks.py
+++ ai_news/buybacks.py
@@ -13,7 +13,7 @@ model = genai.GenerativeModel('gemini-1.5-flash-latest')
 prompt_template="The time now is "
 prompt_template+=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
 
-prompt_template+=""". Based on the following search result, please answer yes or no about whether this constitues a NEW announcement about a share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement is older than 24 hours or simply an update about stock repurchases that already happened, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
+prompt_template+=""" UTC. Based on the following search result, please answer yes or no about whether this constitues a NEW announcement about a share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement simply an update about stock repurchases that already happened, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
 Respond in a format like this: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
 
 Here is the search result: 
@@ -26,9 +26,9 @@ ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='buyback|r
 prompt_template="The time now is "
 prompt_template+=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
 
-prompt_template+=""" UTC. Based on the following search result, please answer yes or no about whether this constitues a NEW announcement of improved guidance, The announcement should say that guidance for an important metric is now higher than previously expected. If the announcement is from before the close of the previous trading day or simply an update about progress on existing guidance, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
+prompt_template+=""" UTC. Based on the following search result, please answer yes or no about whether this constitues a NEW announcement of improved guidance, The announcement should say that guidance for an important metric is now higher than previously expected. If the announcement is simply an update about progress on existing guidance, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
 Respond in this JSON format with time formatted as follows: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
-p
+
 Here is the search result: 
 """
 
