9ec74d5e3665ec91f5e02711f439ecaba0f108ee
asinayev <asinayev@gmail.com>
2024-11-13 16:42:27 -0500

    update the prompts some more

--- ai_news/buybacks.py
+++ ai_news/buybacks.py
@@ -10,11 +10,8 @@ polygon_key = os.environ["POLYGONKEY"]
 genai.configure(api_key=google_key)
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
-prompt_template="The time now is "
-prompt_template+=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-
-prompt_template+=""" UTC. Based on the following search result, please answer yes or no about whether this constitues a NEW announcement about a share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement simply an update about stock repurchases that already happened, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
-Respond in a format like this: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
+prompt_template="""Based on the following search result, please answer yes or no about whether this constitues a NEW announcement about a share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement simply an update about stock repurchases that already happened, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
+Respond in this JSON format: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
 
 Here is the search result: 
 """
@@ -23,11 +20,8 @@ ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='buyback|r
   orTerms='', prompt_template=prompt_template, model=model)
 
 
-prompt_template="The time now is "
-prompt_template+=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-
-prompt_template+=""" UTC. Based on the following search result, please answer yes or no about whether this constitues a NEW announcement of improved guidance, The announcement should say that guidance for an important metric is now higher than previously expected. If the announcement is simply an update about progress on existing guidance, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
-Respond in this JSON format with time formatted as follows: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
+prompt_template="""Based on the following search result, please answer yes or no about whether this constitues a NEW announcement of improved guidance in the newProgram field, The announcement should say that guidance for an important metric is now higher than previously expected. If the announcement is simply an update about progress on existing guidance, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
+Respond in this JSON format: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
 
 Here is the search result: 
 """
