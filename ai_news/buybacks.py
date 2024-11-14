f0e5b0d01cbd8f446419149b67bf639467585664
asinayev <asinayev@gmail.com>
2024-11-14 15:45:10 -0500

    dont read the filename from arg

--- ai_news/buybacks.py
+++ ai_news/buybacks.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import time, datetime
 import ai_reads_search
 import google.generativeai as genai
@@ -7,7 +6,6 @@ import google.generativeai as genai
 my_cse_id = os.environ["SEARCHID"]
 google_key = os.environ["GOOGLEKEY"]
 polygon_key = os.environ["POLYGONKEY"]
-write_to_dir = sys.argv[1]
 
 genai.configure(api_key=google_key)
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
@@ -22,9 +20,9 @@ Here is the search result:
 buyback_description="share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement is simply an update about stock repurchases that already happened, respond no. "
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='buyback|repurchase share|stock', \
-  orTerms='', prompt_template=prompt_template.format(buyback_description), model=model, write_to_dir=write_to_dir)
+  orTerms='', prompt_template=prompt_template.format(buyback_description), model=model, write_to_dir='/tmp/stonksanalysis')
 
 guidance_description="improved guidance in the newProgram field, The announcement should say that guidance for an important metric is now higher (better) than previously expected. If the announcement is simply an update about progress on existing guidance, respond no."
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='improves|raises|increases guidance', \
-  orTerms='', prompt_template=prompt_template.format(guidance_description), model=model, write_to_dir=write_to_dir)
+  orTerms='', prompt_template=prompt_template.format(guidance_description), model=model, write_to_dir='/tmp/stonksanalysis')
