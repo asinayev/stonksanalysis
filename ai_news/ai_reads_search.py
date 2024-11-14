3e8b175c1f1938d7b9087ee866e3638f9ce4cf78
asinayev <asinayev@gmail.com>
2024-11-14 15:43:54 -0500

    write_to_file

--- ai_news/ai_reads_search.py
+++ ai_news/ai_reads_search.py
@@ -1,9 +1,10 @@
 from googleapiclient.discovery import build
 from polygon import RESTClient
+import pandas as pd
 import search_web
 import call_ai
 
-def read_search(google_key, polygon_key, search_id, query, prompt_template, model, **kwargs):
+def read_search(google_key, polygon_key, search_id, query, prompt_template, model, write_to_dir, **kwargs):
   search_service = build("customsearch", "v1", developerKey=google_key).cse()
   
   all_results=search_web.all_search_pages(
@@ -16,16 +17,25 @@ def read_search(google_key, polygon_key, search_id, query, prompt_template, mode
   
   valid_results = call_ai.read_results(all_results, prompt_template, model)
   poly_client = RESTClient(api_key=polygon_key)
-
+  default_data={
+    'date': '03/12/2024',
+    'symbol': '',
+    'strike_price': '',
+    'action': 'BUY',
+    'order_type': 'MKT',
+    'time_in_force': 'OPG',
+    'asset_type': 'liquid'
+  }
   print("############################ MATCHING RESULTS: "+query)
   enriched_results = [call_ai.enrich_result(r, poly_client) for r in valid_results]
-  already_tracked = []
+  already_tracked = pd.DataFrame([default_data])
   for r in enriched_results:
-    if r['match'] and r['companyName'] not in already_tracked:
+    if r['match'] and not (already_tracked['companyName']==r['companyName']).any():
       model_out=model.generate_content("Reformat the following time as YYYY-MM-DD HH:MM using military time:"+ r['timePublished'])
       r['timePublished'] = model_out.text.strip()
       print(r['timePublished'],r['ticker'],r['link'],r['title'])
-      already_tracked.append(r['companyName'])
+      already_tracked=pd.concat([pandas_results, pd.DataFrame([r])], ignore_index=True)
+  pandas_results.to_csv(write_to_dir+query.replace('|','_')+'.csv'
   
   print("############################ OTHER RESULTS: "+query)
   for r in enriched_results:
