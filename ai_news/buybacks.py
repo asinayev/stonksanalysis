b5fa33085a30eb2e8b51e6b03349c234aa8d7c5c
asinayev <asinayev@gmail.com>
2024-11-14 15:05:04 -0500

    Update buybacks.py for writing to directory

--- ai_news/buybacks.py
+++ ai_news/buybacks.py
@@ -1,30 +1,30 @@
-import google.generativeai as genai
 import os
+import sys
 import time, datetime
 import ai_reads_search
+import google.generativeai as genai
 
 my_cse_id = os.environ["SEARCHID"]
 google_key = os.environ["GOOGLEKEY"]
 polygon_key = os.environ["POLYGONKEY"]
+write_to_dir = sys.argv[1]
 
 genai.configure(api_key=google_key)
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
-prompt_template="""Based on the following search result, please answer yes or no about whether this constitues a NEW announcement about a share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement simply an update about stock repurchases that already happened, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
+prompt_template="""Based on the following search result, please answer yes or no about whether this constitues a NEW announcement about a {}
+Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
 Respond in this JSON format: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
 
 Here is the search result: 
 """
+
+buyback_description="share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement is simply an update about stock repurchases that already happened, respond no. "
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='buyback|repurchase share|stock', \
-  orTerms='', prompt_template=prompt_template, model=model)
+  orTerms='', prompt_template=prompt_template.format(buyback_description), model=model, write_to_dir=write_to_dir)
 
-
-prompt_template="""Based on the following search result, please answer yes or no about whether this constitues a NEW announcement of improved guidance in the newProgram field, The announcement should say that guidance for an important metric is now higher than previously expected. If the announcement is simply an update about progress on existing guidance, respond no. Please also answer the full name of the company doing the announcement, the ticker and the time it was published. 
-Respond in this JSON format: {"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"8/12/2024 3:30:00 PM"}
-
-Here is the search result: 
-"""
+guidance_description="improved guidance in the newProgram field, The announcement should say that guidance for an important metric is now higher (better) than previously expected. If the announcement is simply an update about progress on existing guidance, respond no."
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='improves|raises|increases guidance', \
-  orTerms='', prompt_template=prompt_template, model=model)
+  orTerms='', prompt_template=prompt_template.format(guidance_description), model=model, write_to_dir=write_to_dir)
