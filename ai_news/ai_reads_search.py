08c48f3118687f40b5fb99b3733ded6c85044d4f
asinayev <asinayev@gmail.com>
2024-11-25 07:30:59 -0500

    Update ai_reads_search.py

--- ai_news/ai_reads_search.py
+++ ai_news/ai_reads_search.py
@@ -35,14 +35,13 @@ def read_search(google_key, polygon_key, search_id, query, prompt_template, mode
   print(f"{len([x for x in enriched_results if x['match'] ])} matching enriched results")
   already_tracked = pd.DataFrame([default_data])
   for r in enriched_results:
-    if not (already_tracked['companyName']==r['companyName']).any():
-      model_out=model.generate_content("Reformat the following time as YYYY-MM-DD HH:MM using military time (put 00:00 if no time is provided):"+ r['timePublished'])
-      try:
-        r['timePublished'] = model_out.text.strip()
-      except:
-        r['timePublished'] = 'cannot be formatted'
-      if r['match']:
-        print(r['timePublished'],r['ticker'],r['link'],r['title'])
-      already_tracked=pd.concat([already_tracked, pd.DataFrame([r])], ignore_index=True)
+    model_out=model.generate_content("Reformat the following time as YYYY-MM-DD HH:MM using military time (put 00:00 if no time is provided):"+ r['timePublished'])
+    try:
+      r['timePublished'] = model_out.text.strip()
+    except:
+      r['timePublished'] = 'cannot be formatted'
+    if r['match']:
+      print(r['timePublished'],r['ticker'],r['link'],r['title'])
+    already_tracked=pd.concat([already_tracked, pd.DataFrame([r])], ignore_index=True)
   already_tracked.sort_values(["newProgram","timePublished"],ascending=False).to_csv(write_to_dir+query.replace('|','_')+'.csv')
 
