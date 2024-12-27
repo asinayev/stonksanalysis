9d88d2220e0747bb461e84e75c89bd8119bd9f61
asinayev <asinayev@gmail.com>
2024-12-27 10:04:03 -0500

    number of results as argument

--- ai_news/search_web.py
+++ ai_news/search_web.py
@@ -1,8 +1,8 @@
-def all_search_pages(service, cse_id, **kwargs):
+def all_search_pages(service, cse_id, n_results=90, **kwargs):
   all_results=[]
   results=['items']
   search_i=1
-  while 'items' in results and search_i<90:
+  while 'items' in results and search_i<n_results:
     results = service.list(cx=cse_id, start=search_i, **kwargs).execute()
     if 'items' in results:
       all_results+=results['items']
