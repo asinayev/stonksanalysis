3c7040a861f4520292776bcea480aee2384188b0
asinayev <asinayev@gmail.com>
2025-03-17 12:48:17 -0400

    Must mention in title

--- ai_news/buybacks.py
+++ ai_news/buybacks.py
@@ -21,7 +21,7 @@ model = genai.GenerativeModel(
   )
 
 prompt_template="""
-Determine if the following search result is a NEW announcement about a {}. Answer 'yes' or 'no'.
+Determine if the following search result is a NEW announcement about a {}  If the title doesn't refer to the company, respond no. Answer 'yes' or 'no'.
 
 For your response, also extract the following (except the quote, provide info if it is known even if it is not in the announcement):
 - Company Full Name
@@ -29,11 +29,9 @@ For your response, also extract the following (except the quote, provide info if
 - Publication Date and Time
 - If available, a key quote from the search result that supports your 'yes'/'no' determination.
 
-Publication date and time may be found in the snippet, body or metadata like pagemap metalogs in the "date" field. It may appear in any format like 'days ago' or a datetime with timezone like '1999-07-01T23:21:10-5:00' or other. Reformat to YYYY-MM-DD HH:MM using military time (put 00:00 if no time is provided). Do not convert timezones, so if it says +3:00, that refers to the timezone and you can ignore that.
+Locate the publication time in the search result's snippet, body or metadata (metadata like pagemap metalogs in the "date" field). It may appear in any format like 'days ago' or a datetime with timezone like '1999-07-01T23:21:10-5:00' or other. Reformat to YYYY-MM-DD HH:MM using military time (put 00:00 if no time is provided). Do not convert timezones, so if it says +3:00, that refers to the timezone and you can ignore that.
 
 If any of these details cannot be found, use "UNKNOWN".
-
-Locate the publication time in the search result's snippet, body, or metadata (e.g., 'date' field).
 
 Respond in JSON format as shown in this example:
 {{"newProgram":"No","companyName":"Microsoft Corporation","ticker":"MSFT","timePublished":"2024-08-12 15:30","quote":"In June, the board announced plans for a new shareholder incentive..."}}
@@ -41,7 +39,7 @@ Respond in JSON format as shown in this example:
 Search result:
 """
 
-buyback_description="share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement is simply an update about stock repurchases that already happened, respond no. "
+buyback_description="share buyback or share repurchase program which says more stocks will be repurchased in the future. If the announcement is simply an update about stock repurchases that already happened, respond no."
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, query='buyback|repurchase share|stock', \
   orTerms='', prompt_template=prompt_template.format(buyback_description), model=model, write_to_dir='/tmp/stonksanalysis/')
