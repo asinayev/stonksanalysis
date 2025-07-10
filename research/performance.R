541fd453aa842523be791edc51e0c233c8ae6c24
asinayev <asinayev@gmail.com>
2025-07-10 11:27:24 -0400

    improved performance features

--- research/performance.R
+++ research/performance.R
@@ -36,14 +36,13 @@ performance=function(date,outcome,days_held,symbol,sell_date=date, no_doubling=F
   results_daily[,drawdown_i:=cumsum(drawdown==0)]
   results_daily[,n_held:=cumsum(trades-n_sold)]
   results_overall= results_daily[,.(
-    average=round(sum(outcome)/sum(trades),3),
-    drawdown=round(min(drawdown,na.rm=T),1),
-    total=round(sum(outcome),1),
-    trades=sum(trades),
-    days_traded=length(unique(ifelse(trades>0,date,NA))),
-    max_held=max(n_held)
-  ), 
-  year(date) ]%>%
+      average=round(sum(outcome)/sum(trades),3),
+      drawdown=round(min(drawdown,na.rm=T),1),
+      total=round(sum(outcome),1),
+      trades=sum(trades),
+      days_traded=length(unique(ifelse(trades>0,date,NA))),
+      max_held=max(n_held)
+    ), year(date) ]%>%
     merge(results[,.(avg_days_held=mean(days_held),
                      stocks_traded=length(unique(symbol))),
                   year(date)])
@@ -55,9 +54,10 @@ performance=function(date,outcome,days_held,symbol,sell_date=date, no_doubling=F
   abline(v = seq(as.Date("2000-01-01"), as.Date("2030-12-31"), by = "year"),lty=2,col='gray')
   abline(v = seq(as.Date("2000-01-01"), as.Date("2030-12-31"), by = "month"),lty=3,col='lightgray')
   max_drawdown_days = max(results_daily[,.(datediff=max(date)-min(date)),drawdown_i][,datediff])
-  print(results_overall[,.(avg_year=mean(average), 
+  print(results_overall[,.(yr_total_per_held=mean(total)/max(max_held), 
                            avg_trade=sum(total)/sum(trades),
                            drawdown=min(drawdown), 
+                           drawdown_per_held=min(drawdown)/max(max_held), 
                            drawdown_days=max(max_drawdown_days),
                            days_traded=sum(days_traded),
                            max_held=max(max_held))])
