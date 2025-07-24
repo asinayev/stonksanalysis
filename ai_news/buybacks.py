98bdcd9eef6ca2eaee19d6c46f795fec549e933c
asinayev <asinayev@gmail.com>
2025-07-23 22:10:20 -0400

    Update buybacks.py

--- ai_news/buybacks.py
+++ ai_news/buybacks.py
@@ -44,7 +44,7 @@ Search result:
 buyback_description="an announcement of a share buyback or share repurchase program. If the announcement does not say that stock will be repurchased or bought back IN THE FUTURE, (e.g., is simply an update about stock repurchases that already happened, or reminds shareholders of a previously announced repurchase), respond no."
 buyback_parameters={'search_query':'buyback|repurchase share|stock',
                     'prompt_template':prompt_template.format(buyback_description), 
-                    'min_close':5, 'min_volume':20000, 'max_market_cap':10000000000, 'min_overnight_pchange':-1.75, 'max_overnight_pchange':9}
+                    'min_close':5, 'min_volume':20000, 'max_market_cap':10000000000, 'min_overnight_pchange':-2.2, 'max_overnight_pchange':9}
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, orTerms='', model=model, write_to_dir='/tmp/stonksanalysis/', \
                            parameters=buyback_parameters)
@@ -52,7 +52,7 @@ ai_reads_search.read_search(google_key, polygon_key, my_cse_id, orTerms='', mode
 guidance_description="improved guidance in the newProgram field, The announcement should say that guidance for an important metric is now higher (better) than previously expected. If the announcement is simply an update about progress on existing guidance, respond no."
 guidance_parameters={'search_query':'improves|raises|increases guidance|outlook',
                      'prompt_template':prompt_template.format(guidance_description), 
-                     'min_close':5, 'min_volume':20000, 'max_market_cap':100000000000, 'min_overnight_pchange':0, 'max_overnight_pchange':20}
+                     'min_close':5, 'min_volume':20000, 'max_market_cap':100000000000, 'min_overnight_pchange':-0.2, 'max_overnight_pchange':20}
 
 ai_reads_search.read_search(google_key, polygon_key, my_cse_id, orTerms='', model=model, write_to_dir='/tmp/stonksanalysis/', \
                            parameters=guidance_parameters)
